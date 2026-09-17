6413f1a5e87f926a49fd53aff4cde2bb35e685ae
nakul465 <nakularora465@gmail.com>
2026-09-17 22:44:30 +0530

    UC2: Show the total number of deaths in each country, including provinces/states, for a given date.

--- Covid_DB_Project/queries_and_solutions/solutions.sql
+++ Covid_DB_Project/queries_and_solutions/solutions.sql
@@ -11,3 +11,15 @@ WHERE g.report_date = '2021-03-31'
 ORDER BY g.confirmed DESC
 LIMIT 1;
 
+-- UC2 : 
+-- Show the total number of deaths in each country, including provinces/states, for a given date.
+
+SELECT c.name,s.name,SUM(cs.deaths)
+FROM country as c
+JOIN covid_case_stats as cs
+	on c.country_id=cs.country_id
+JOIN state as s
+	on cs.state_id=s.state_id
+WHERE cs.report_date='2021-03-31'
+GROUP BY c.name,s.name
+ORDER BY SUM(cs.deaths) DESC;
